07a599cabac6e28eb22938079bcf727bacb8918e
Mjtoffey <mjtoffey@gmail.com>
2023-08-24 21:20:41 +0000

    argh

--- whynotme/urls.py
+++ whynotme/urls.py
@@ -16,7 +16,7 @@ router.register(r'games', GameViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('user-log/<int:user_id>/', FilteredUserLogListView.as_view(), name='filtered_user_logs'),
-    path('user/signup/', UserCreate.as_view(), name="create_user"),
+    path('user/register/', UserCreate.as_view(), name="create_user"),
     path('huh/<int:pk>/', UserDetail.as_view(), name="get_user_details"),
     path('user/login/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),
     path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
